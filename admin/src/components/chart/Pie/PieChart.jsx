9678094e147ccafcebc288b1f1541dad6c933710
Mudita <gtdteam20@gmail.com>
2026-02-14 16:04:03 +0530

    responsive ui

--- admin/src/components/chart/Pie/PieChart.jsx
+++ admin/src/components/chart/Pie/PieChart.jsx
@@ -35,6 +35,125 @@ const PieChart = ({ data }) => {
     ],
   };
 
+  const getOrCreateTooltip = (chart) => {
+    let tooltipEl = chart.canvas.parentNode.querySelector("div.chartjs-tooltip");
+
+    if (!tooltipEl) {
+      tooltipEl = document.createElement("div");
+      tooltipEl.classList.add("chartjs-tooltip");
+      tooltipEl.style.background = "#020617";
+      tooltipEl.style.borderRadius = "8px";
+      tooltipEl.style.color = "white";
+      tooltipEl.style.opacity = 1;
+      tooltipEl.style.pointerEvents = "none";
+      tooltipEl.style.position = "absolute";
+      tooltipEl.style.transform = "translate(-50%, 0)";
+      tooltipEl.style.transition = "all .1s ease";
+      tooltipEl.style.zIndex = "100";
+      tooltipEl.style.minWidth = "180px";
+      tooltipEl.style.padding = "8px 12px";
+      tooltipEl.style.boxShadow = "0 4px 6px -1px rgba(0, 0, 0, 0.1)";
+
+      const table = document.createElement("table");
+      table.style.margin = "0px";
+
+      tooltipEl.appendChild(table);
+      chart.canvas.parentNode.appendChild(tooltipEl);
+    }
+
+    return tooltipEl;
+  };
+
+  const externalTooltipHandler = (context) => {
+    // Tooltip Element
+    const { chart, tooltip } = context;
+    const tooltipEl = getOrCreateTooltip(chart);
+
+    // Hide if no tooltip
+    if (tooltip.opacity === 0) {
+      tooltipEl.style.opacity = 0;
+      return;
+    }
+
+    // Set Text
+    if (tooltip.body) {
+      const titleLines = tooltip.title || [];
+      const bodyLines = tooltip.body.map((b) => b.lines);
+
+      const tableHead = document.createElement("thead");
+
+      titleLines.forEach((title) => {
+        const tr = document.createElement("tr");
+        tr.style.borderWidth = 0;
+        const th = document.createElement("th");
+        th.style.borderWidth = 0;
+        th.style.textAlign = "left";
+        th.style.fontSize = "13px";
+        th.style.color = "#fff";
+        th.style.paddingBottom = "4px";
+
+        const text = document.createTextNode(title);
+        th.appendChild(text);
+        tr.appendChild(th);
+        tableHead.appendChild(tr);
+      });
+
+      const tableBody = document.createElement("tbody");
+      bodyLines.forEach((body, i) => {
+        const colors = tooltip.labelColors[i];
+
+        const span = document.createElement("span");
+        span.style.background = colors.backgroundColor;
+        span.style.borderColor = colors.borderColor;
+        span.style.borderWidth = "2px";
+        span.style.marginRight = "8px";
+        span.style.height = "12px";
+        span.style.width = "12px";
+        span.style.display = "inline-block";
+        span.style.borderRadius = "2px";
+
+        const tr = document.createElement("tr");
+        tr.style.backgroundColor = "inherit";
+        tr.style.borderWidth = 0;
+
+        const td = document.createElement("td");
+        td.style.borderWidth = 0;
+        td.style.display = "flex";
+        td.style.alignItems = "center";
+        td.style.fontSize = "13px";
+
+        const text = document.createTextNode(body);
+
+        td.appendChild(span);
+        td.appendChild(text);
+        tr.appendChild(td);
+        tableBody.appendChild(tr);
+      });
+
+      const tableRoot = tooltipEl.querySelector("table");
+
+      // Remove old children
+      while (tableRoot.firstChild) {
+        tableRoot.firstChild.remove();
+      }
+
+      // Add new children
+      tableRoot.appendChild(tableHead);
+      tableRoot.appendChild(tableBody);
+    }
+
+    const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
+
+    // Display, position, and set styles for font
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.left = positionX + tooltip.caretX + "px";
+    tooltipEl.style.top = positionY + tooltip.caretY + "px";
+    tooltipEl.style.fontFamily = tooltip.options.bodyFont.family;
+    tooltipEl.style.fontSize = tooltip.options.bodyFont.size + "px";
+    tooltipEl.style.fontStyle = tooltip.options.bodyFont.style;
+    // tooltipEl.style.padding = tooltip.options.padding + "px " + tooltip.options.padding + "px";
+  };
+
   const chartOptions = {
     responsive: true,
     maintainAspectRatio: false,
@@ -42,9 +161,8 @@ const PieChart = ({ data }) => {
     plugins: {
       legend: { display: false },
       tooltip: {
-        backgroundColor: "#020617",
-        padding: 12,
-        cornerRadius: 10,
+        enabled: false,
+        external: externalTooltipHandler,
         callbacks: {
           label: (ctx) => `${ctx.parsed} Units Sold`,
         },
